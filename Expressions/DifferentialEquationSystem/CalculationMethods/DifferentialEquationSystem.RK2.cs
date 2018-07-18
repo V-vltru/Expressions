151114106a1d753ae4858adff3b7290b0ba9bcc7
Vladimir Trunov <v-vltrun@microsoft.com>
2018-07-18 16:20:07 +0300

    Начал разработку метода РК2

---
+++ Expressions/DifferentialEquationSystem/CalculationMethods/DifferentialEquationSystem.RK2.cs
@@ -0,0 +1,22 @@
+﻿namespace DifferentialEquationSystem
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Expressions.Models;
+
+    public partial class DifferentialEquationSystem
+    {
+        private List<InitVariable> RK2Async(List<List<InitVariable>> variablesAtAllStep)
+        {
+            // Put left variables, constants and time variable in the one containier
+            List<Variable> allVars;
+            List<Variable> currentLeftVariables = new List<Variable>();
+            List<Variable> nextLeftVariables = new List<Variable>();
+
+            return null;
+        }
+    }
+}
