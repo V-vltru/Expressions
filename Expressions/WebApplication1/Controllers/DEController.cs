c87a728bd3a00bcf80d8e8fc91195f396a80998e
Vladimir Trunov <v-vltrun@microsoft.com>
2019-04-07 19:38:11 +0300

    Initial commit.

---
+++ Expressions/WebApplication1/Controllers/DEController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DEController : Controller
+    {
+        // GET: DE
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Expressions/WebApplication1/Controllers/IntegralController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using Integral;
+
+namespace WebApplication1.Controllers
+{
+    public class IntegralController : Controller
+    {
+        // GET: Integral
+        [HttpPost]
+        public string Calculate(IntegralInput input)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (CalculationType type in input.Types)
+            {
+                Integral.Integral integral = new Integral.Integral(input.Integrand, input.StartValue, 
+                    input.EndValue, input.IterationsNumber, input.ParameterName);
+
+                double result = integral.Calculate(type);
+
+                stringBuilder.AppendLine($"{type.ToString()}: {result}");
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
